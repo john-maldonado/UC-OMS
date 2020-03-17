5b33b9e8b44b59a0b0981af184af749918e8f0ad
John Maldonado <jomaldonado209@gmail.com>
2020-03-17 13:50:21 +0000

    Changed db IP addresses to docker bridge host ip

--- uc_oms_db_connections.py
+++ uc_oms_db_connections.py
@@ -2,7 +2,7 @@ import mysql.connector
 
 def loginDBConnect(): # Creates connection to uc_db with uc_login user
     connection = mysql.connector.connect(
-    host="localhost",
+    host="172.17.0.1",
     user="uc_login",
     passwd="^6h@fJV9BzzkKjBc",
     database="uc_db"
@@ -11,7 +11,7 @@ def loginDBConnect(): # Creates connection to uc_db with uc_login user
 
 def ordersDBConnect():
   connection = mysql.connector.connect(
-    host="localhost",
+    host="172.17.0.1",
     user="uc_orders",
     passwd="-dJS+2!mFRexjbP?",
     database="uc_db"
